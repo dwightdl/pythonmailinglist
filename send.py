7a0634e98becf15648a43b14da37c2bd8bf1ed08
david <sbb.david@gmail.com>
2018-10-13 10:19:57 -0700

    created list to store email addresses and html template

--- send.py
+++ send.py
@@ -1,17 +1,49 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from logininfo import username, password
 
-s = smtplib.SMTP()
+class AWSSender:
 
-s.connect('email-smtp.us-west-2.amazonaws.com', 587)
+	s = smtplib.SMTP()
 
-s.starttls()
+	s.connect('email-smtp.us-west-2.amazonaws.com', 587)
 
-#username and password
-s.login(username, password)
+	s.starttls()
 
-msg = 'From: sbb.david@gmail.com\nTo: david@submetersolutions.com\nSubject: Test email\n\n%s' % body
+	#username and password
+	s.login(username, password)
 
-body = 'This is a test email'
+	def quit(self):
+		s.quit()
 
-s.sendmail('sbb.david@gmail.com', 'david@submetersolutions.com', msg)
+	def send(self, email, me):
+		msg = MIMEMultipart('alternative')
+		msg['Subject'] = "Hello World!"
+		msg['From'] = me
+		msg['To'] = email
+
+		# Create the body of the message (plain text and an HTML version)
+		text = "Email send using a template!"
+		html = open('template.html', 'r').read()
+
+		html = html.replace('{{ content }}', text)
+
+		# Record the MIME types of both parts - text/plain and text/html
+		part1 = MIMEText(text, 'plain')
+		part2 = MIMEText(html, 'html')
+
+		# Attach parts into message container
+		msg.attach(part1)
+		msg.attach(part2)
+
+		self.s.sendmail(me, email, msg.as_string())
+		print("Sent email to " + email, end="")
+
+emaillist = open('list.txt', 'r').readlines()
+
+aws = AWSSender()
+
+for email in emaillist:
+	aws.send(email, "sbb.david@gmail.com")
+
